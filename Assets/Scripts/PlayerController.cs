98868e49a3d754a8b5fcbc2535656dd46e541635
MaciekBlady <maciek.bla@gmail.com>
2020-11-11 20:48:34 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D m_Rigidbody2D;
+
+    [SerializeField]
+    private float m_MovementSpeed = 3f;
+
+    [SerializeField]
+    private float m_JumpForce = 500f;
+
+    private bool m_Jump = false;
+
+    private float m_Movement = 0f;
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateMovement();
+    }
+
+    private void GetInput()
+    {
+        m_Movement = Input.GetAxis("Horizontal") * m_MovementSpeed;
+
+        m_Jump = Input.GetButtonDown("Jump");
+    }
+
+    private void UpdateMovement()
+    {
+        m_Rigidbody2D.velocity = new Vector2(m_Movement, m_Rigidbody2D.velocity.y);
+
+        if (m_Jump)
+        {
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 RotationDelta;
+
+    private Transform m_Transform;
+
+    private void Awake()
+    {
+        m_Transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Transform.Rotate(RotationDelta);
+    }
+}
